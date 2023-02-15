a8cd67ce46bf04f91c7c7bd105b2b25854d3973c
vshanmug2022 <veera_2001@yahoo.com>
2023-02-15 16:37:51 -0700

    job_id

--- jobs/app.py
+++ jobs/app.py
@@ -36,6 +36,6 @@ def jobs():
     return render_template('index.html', jobs=jobs)
 
 @app.route('/job/<job_id>')
-def job():
+def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
