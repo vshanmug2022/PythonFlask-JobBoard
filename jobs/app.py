7ceb1d962dca9514fbb2c8163bf3941b8542912f
vshanmug2022 <veera_2001@yahoo.com>
2023-02-15 15:12:14 -0700

    close_connection

--- jobs/app.py
+++ jobs/app.py
@@ -9,6 +9,28 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql():
+    connection = open_connection(sql, values=(), commit=False, single=False)
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not NOne:
+        connection.close()
+
+
+
 
 @app.route('/')
 @app.route('/jobs')
