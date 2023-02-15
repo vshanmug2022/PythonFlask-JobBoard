de098a8a9445fd243e21b3a73ff4ec14de967ad9
vshanmug2022 <veera_2001@yahoo.com>
2023-02-15 15:15:12 -0700

    execute_sql

--- jobs/app.py
+++ jobs/app.py
@@ -12,8 +12,8 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql():
-    connection = open_connection(sql, values=(), commit=False, single=False)
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
